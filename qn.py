801424a8098f0ba2bd3935af69e23f51069817e0
JF <sid_fj@hotmail.com>
2014-12-23 18:36:44 +0800

    Create qn.py

---
+++ qn.py
@@ -0,0 +1,42 @@
+__author__ = 'jfang'
+
+import qiniu
+from qiniu import *
+
+access_key = 'dbPhcCoD5oQKeaIays_NkoYVsNwQ-kuzEzuI-whv'
+secret_key = 'gbIZYLlYMkkrm55u2p7TXsL_Qx942q8lY_3gEHcG'
+bucket_name = 'fanfan-photo'
+
+q = Auth(access_key, secret_key)
+
+# 直接上传二进制流
+
+key = 'a\\b\\c"你好'
+data = 'hello bubby!'
+token = q.upload_token(bucket_name)
+ret, info = put_data(token, key, data)
+print(info)
+assert ret['key'] == key
+
+key = ''
+data = 'hello bubby!'
+token = q.upload_token(bucket_name, key)
+ret, info = put_data(token, key, data, check_crc=True)
+print(info)
+assert ret['key'] == key
+
+
+# 上传本地文件
+
+localfile = __file__
+key = 'test_file.py'
+mime_type = "text/plain"
+params = {'x:a': 'a'}
+
+token = q.upload_token(bucket_name, key)
+ret, info = put_file(token, key, localfile, mime_type=mime_type, check_crc=True)
+print(info)
+assert ret['key'] == key
+assert ret['hash'] == etag(localfile)
+
+
