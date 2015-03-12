9a2653f4065f2798c7c5788239e41586e97e5962
Jian Fang <jian.fang.gm@gmail.com>
2015-03-12 18:37:29 +0800

    Support paginate for GET posts/

--- app/api_1_0/posts.py
+++ app/api_1_0/posts.py
@@ -1,7 +1,7 @@
 __author__ = 'jfang'
 
 from bson import ObjectId
-from flask import request, jsonify
+from flask import request, jsonify, current_app
 from ..models import Post
 from . import api
 
@@ -18,11 +18,23 @@ from . import api
 
 @api.route('/posts/', methods=['GET'])
 def get_posts():
-    posts = []
-    for post in Post.query.filter():
-        if post is not None:
-            posts.append(post.to_json())
-    return jsonify({'posts': posts})
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.paginate(page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'], error_out=False)
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = page-1
+    next = None
+    if pagination.has_next:
+        next = page+1
+    return jsonify({
+        'posts': [post.to_json() for post in posts],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total,
+        'pages': pagination.pages
+    })
+
 
 @api.route('/posts/<id>', methods=['GET'])
 def get_post(id):
--- config/development.py
+++ config/development.py
@@ -4,3 +4,4 @@ DEBUG = True
 IGNORE_AUTH = True
 SECRET_KEY = 'top-secret!'
 MONGOALCHEMY_DATABASE = 'appdb_dev'
+FLASKY_POSTS_PER_PAGE = 5
